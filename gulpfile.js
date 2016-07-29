b2e96b0149ddf075a637b20fa333473a9870874a
Jean Pierre Dudey <jeandudey@hotmail.com>
2016-07-29 12:50:23 -0400

    feat(Jade): Add jade template engine

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var csso = require('gulp-csso');
+var jade = require('gulp-jade');
 
 gulp.task('build', function() {
-    return gulp.src("assets/css/page.css")
+    gulp.src("./assets/css/page.css")
         .pipe(csso())
         .pipe(gulp.dest('./build/css'));
+
+    gulp.src("./jade/index.jade")
+        .pipe(jade())
+        .pipe(gulp.dest("./"))
 });
