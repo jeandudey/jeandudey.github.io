aece7f2a7d6ea85eb5c48e70e2e1fbea680d531d
Jean Pierre Dudey <jeandudey@hotmail.com>
2016-07-29 13:10:46 -0400

    feat(build): Use optimized CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ var csso = require('gulp-csso');
 var pug = require('gulp-pug');
 
 gulp.task('build', function() {
-    gulp.src("./assets/css/page.css")
-        .pipe(csso())
-        .pipe(gulp.dest('./build/css'));
+  gulp.src("./assets/css/page.css")
+    .pipe(csso())
+    .pipe(gulp.dest('./build/css'));
 
-    gulp.src("./pug/index.pug")
-        .pipe(pug())
-        .pipe(gulp.dest("./"))
+  gulp.src("./pug/index.pug")
+    .pipe(pug({
+      locals: {
+          css_file: "./build/css/page.css",
+      }
+    }))
+    .pipe(gulp.dest("./"))
 });
